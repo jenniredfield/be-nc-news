4edcf2d8c2895f5bed71a39d23859802c109286b
Jen Bird <jenniredfield@hotmail.com>
2018-02-06 17:06:16 +0000

    Implement users router

---
+++ routes/users.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+const usersRouter = require('express').Router();
+const findUser = require('../controllers/users.controller')
+
+usersRouter.route('/:username')
+    .get(findUser)
+
+module.exports = usersRouter;
