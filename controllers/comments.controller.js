93e5ceddef0a34bef5d7fe5242a2f95e8210f53c
Jen Bird <jenniredfield@hotmail.com>
2018-02-09 17:22:59 +0000

    Added tests for the updateCommentVote errorHandling functions

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -3,71 +3,84 @@ const Articles = require('../models/articles')
 const Comments = require('../models/comments');
 const models = require('../models/models')
 
-function updateCommentVote(req,res,next){
-    
-        const id = req.params.comment_id;
-        const query = req.query.vote;
- 
-       return Comments.findById(id)
-            .then(comment => {
-             
-                return comment.votes;
-            })
-            .then(newVotes => {
-        
-                if (query === 'up') {
-                    newVotes += 1;
-                }
-                else newVotes -= 1;
-                return Comments.update({_id: id}, {votes: newVotes})
-            }).then(() => {
-                return Comments.findById(id)
-            }).then((comment) => {
-             
-                    res.send(comment)
-            });
+function updateCommentVote(req, res, next) {
+
+    const id = req.params.comment_id;
+    const query = req.query.vote;
+
+    if (req.query.vote === undefined) {
+        next({ statusCode: 400, message: "Please provide a valid query, ie vote=up" })
+    }
+
+    const querys = ["up", "down"]
+
+    if (!querys.includes(query)) {
+        next({ statusCode: 400, message: "Please provide a valid query format,ie vote=up or vote=down" })
+    }
+
+
+    return Comments.findById(id)
+        .then(comment => {
+
+            return comment.votes;
+        })
+        .then(newVotes => {
+
+            if (query === 'up') {
+                newVotes += 1;
+            }
+            else newVotes -= 1;
+            return Comments.update({ _id: id }, { votes: newVotes })
+        }).then(() => {
+            return Comments.findById(id)
+        }).then((comment) => {
+
+            res.status(202).send(comment)
+        }).catch((error)=> {
+            next({ statusCode: 500, message: "Unable to update Vote" })
+        })
 
 }
 
 
-function deleteComment(req,res,next){
+function deleteComment(req, res, next) {
 
     const id = req.params.comment_id;
 
     Comments.findById(id)
-    .then(comment =>{
-        if(comment.created_by === 'northcoder')
-          return Comments.findByIdAndRemove(id)
+        .then(comment => {
+            if (comment.created_by === 'northcoder')
+                return Comments.findByIdAndRemove(id)
 
-    }).then(comment => {
+        }).then(comment => {
 
-        res.status(202).send(comment);
-    })
+            res.status(202).send(comment);
+        })
 
 
 }
 
-function findCommentById(req,res,next){
+function findCommentById(req, res, next) {
 
     const id = req.params.comment_id;
 
     return Comments.findById(id)
-    .then(comment => {
-        res.send(comment);
-    });
+        .then(comment => {
+            res.send(comment);
+        });
 
 }
 
-function getAllComments(req,res,next){
+function getAllComments(req, res, next) {
 
 
     return Comments.find()
-    .then(comments => {
-     
-        res.send(comments);
-    });
+        .then(comments => {
+
+            res.send(comments);
+        });
 
 }
 
 
-module.exports = {updateCommentVote, deleteComment, findCommentById, getAllComments};
+module.exports = { updateCommentVote, deleteComment, findCommentById, getAllComments };
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -59,166 +59,114 @@ describe('API endpoints', () => {
                     expect(res.body.comments.length).to.equal(2);
                     return;
                 });
-            });
-
-            it('GET getArticleById returns the article of Id provided', () => {
-
-                const articleId = docs.articles[0]._id;
-
-                return request
-
-                    .get(`/api/articles/${articleId}`)
-                    .expect(200)
-                    .then(res => {
-       
-                        expect(res.body.article).to.be.an('object');
-                        return;
-                    });
-          
-            });
-
-            /*** ERROR HANDLING */
-            it('GET article by ID with non existent ID returns status 404', () => {
-
-                const articleId = "5a7d980c6a5d660aecab81d0"
-
-                return request
-
-                    .get(`/api/articles/${articleId}/comments`)
-                    .expect(404)
-                    .then(res => {
-                        expect(res.body).to.be.an('object');
-                        expect(res.body.message).to.equal('Not found');
-                        return;
-                    });
-
-            });
-
-            
-            /*** ERROR HANDLING */
-            it('GET article by ID with wrong article ID returns status 400', () => {
-
-                const articleId = "banana"
-
-                return request
-
-                    .get(`/api/articles/${articleId}/comments`)
-                    .expect(400)
-                    .then(res => {
-                        expect(res.body).to.be.an('object');
-                        expect(res.body.message).to.equal('Invalid ID.');
-                        return;
-                    });
-
-            });
-
         });
 
-        it('POST should post a new comment on the article from ID provided', () => {
+        it('GET getArticleById returns the article of Id provided', () => {
 
             const articleId = docs.articles[0]._id;
 
             return request
 
-                .post(`/api/articles/${articleId}/comments`)
-                .send({
-                    comment: "this is a new comment"
-                })
-                .expect(201)
+                .get(`/api/articles/${articleId}`)
+                .expect(200)
                 .then(res => {
 
-                    expect(res.body).to.be.an('object')
-                    expect(res.body.body).to.equal("this is a new comment")
-                    return request
-                        .get(`/api/articles/${articleId}/comments`)
-                        .then(res => {
-
-                            expect(res.body.comments.length).to.equal(3);
-                            return;
-                        })
+                    expect(res.body.article).to.be.an('object');
+                    return;
                 });
 
         });
 
-        it('POST does not post a new comment if the article ID provided is nonexistent ', () => {
+        /*** ERROR HANDLING */
+        it('GET article by ID with non existent ID returns status 404', () => {
 
             const articleId = "5a7d980c6a5d660aecab81d0"
 
             return request
 
-                .post(`/api/articles/${articleId}/comments`)
-                .send({
-                    comment: "this is a new comment"
-                })
+                .get(`/api/articles/${articleId}/comments`)
                 .expect(404)
                 .then(res => {
-
-                    expect(res.body).to.be.an('object')
-                    expect(res.body.message).to.equal("Article not found, check ID")
+                    expect(res.body).to.be.an('object');
+                    expect(res.body.message).to.equal('Not found');
+                    return;
                 });
 
         });
 
-        describe('/api/articles/:article_id', () => {
-            it('PUT should increase the votes of an article by one', () => {
 
-                const articleId = docs.articles[0]._id;
-                const votes = docs.articles[0].votes;
+        /*** ERROR HANDLING */
+        it('GET article by ID with wrong article ID returns status 400', () => {
 
-                return request
-                    .put(`/api/articles/${articleId}?vote=up`)
-                    .expect(202)
-                    .then(res => {
-
-                        expect(res.body).to.be.an('object')
-
-                        return request
-                            .get(`/api/articles/${articleId}`)
-                    }).then(res => {
-
-                        expect(res.body.article.votes).to.equal(1);
-                    })
-            })
-
-            it('PUT returns with error message if query provided was mispelled', () => {
-
-                const articleId = docs.articles[0]._id;
-                const votes = docs.articles[0].votes;
-
-                return request
-                    .put(`/api/articles/${articleId}?ote=up`)
-                    .expect(400)
-                    .then(res => {
-
-                        expect(res.body).to.be.an('object')
-                        expect(res.body.message).to.equal("Please provide a valid query, ie vote=up")
-                    });
-            })
-        })
-
-        it('PUT returns with error message if query value provided is invalid', () => {
-
-            const articleId = docs.articles[0]._id;
-            const votes = docs.articles[0].votes;
+            const articleId = "banana"
 
             return request
-                .put(`/api/articles/${articleId}?vote=banana`)
+
+                .get(`/api/articles/${articleId}/comments`)
                 .expect(400)
                 .then(res => {
+                    expect(res.body).to.be.an('object');
+                    expect(res.body.message).to.equal('Invalid ID.');
+                    return;
+                });
 
-                    expect(res.body).to.be.an('object')
-                    expect(res.body.message).to.equal("Please provide a valid query format,ie vote=up or vote=down")
+        });
 
-                })
-        })
+    });
 
-        it('PUT should decrease the votes of an article by one with down query', () => {
+    it('POST should post a new comment on the article from ID provided', () => {
+
+        const articleId = docs.articles[0]._id;
+
+        return request
+
+            .post(`/api/articles/${articleId}/comments`)
+            .send({
+                comment: "this is a new comment"
+            })
+            .expect(201)
+            .then(res => {
+
+                expect(res.body).to.be.an('object')
+                expect(res.body.body).to.equal("this is a new comment")
+                return request
+                    .get(`/api/articles/${articleId}/comments`)
+                    .then(res => {
+
+                        expect(res.body.comments.length).to.equal(3);
+                        return;
+                    })
+            });
+
+    });
+    /*** ERROR HANDLING */
+    it('POST does not post a new comment if the article ID provided is nonexistent ', () => {
+
+        const articleId = "5a7d980c6a5d660aecab81d0"
+
+        return request
+
+            .post(`/api/articles/${articleId}/comments`)
+            .send({
+                comment: "this is a new comment"
+            })
+            .expect(404)
+            .then(res => {
+
+                expect(res.body).to.be.an('object')
+                expect(res.body.message).to.equal("Article not found, check ID")
+            });
+
+    });
+
+    describe('/api/articles/:article_id', () => {
+        it('PUT should increase the votes of an article by one', () => {
 
             const articleId = docs.articles[0]._id;
             const votes = docs.articles[0].votes;
 
             return request
-                .put(`/api/articles/${articleId}?vote=down`)
+                .put(`/api/articles/${articleId}?vote=up`)
                 .expect(202)
                 .then(res => {
 
@@ -227,98 +175,168 @@ describe('API endpoints', () => {
                     return request
                         .get(`/api/articles/${articleId}`)
                 }).then(res => {
-                    expect(res.body.article.votes).to.equal(-1);
+
+                    expect(res.body.article.votes).to.equal(1);
                 })
         })
+        /*** ERROR HANDLING */
+        it('PUT returns with error message if query provided was mispelled', () => {
 
-        it('PUT should increase the votes of an comment by one', () => {
-
-            const commentId = docs.comments[0]._id;
-            const votes = docs.comments[0].votes;
+            const articleId = docs.articles[0]._id;
+            const votes = docs.articles[0].votes;
 
             return request
-                .put(`/api/comments/${commentId}?vote=up`)
-                .expect(202)
+                .put(`/api/articles/${articleId}?ote=up`)
+                .expect(400)
                 .then(res => {
 
                     expect(res.body).to.be.an('object')
-
-                    return request
-                        .get(`/api/comments/${commentId}`)
-                }).then(res => {
-                    expect(res.body.votes).to.equal(1);
-                })
-        });
-
-        it('PUT should decrease the votes of an comment by one with down query', () => {
-
-            const commentId = docs.comments[0]._id;
-            const votes = docs.comments[0].votes;
-
-            return request
-                .put(`/api/comments/${commentId}?vote=down`)
-                .expect(202)
-                .then(res => {
-
-                    expect(res.body).to.be.an('object')
-
-                    return request
-                        .get(`/api/comments/${commentId}`)
-                }).then(res => {
-                    expect(res.body.votes).to.equal(-1);
-                })
-        });
-
-        // it.only('PUT updateVote replies with status 400 if request contains bad query string', () => {
-
-        //     const commentId = docs.comments[0]._id;
-        //     const votes = docs.comments[0].votes;
-
-        //     return request
-        //         .put(`/api/comments/${commentId}?ote=down`)
-        //         .expect(400)
-        //         .then(res => {
-
-        //             expect(res.body).to.be.an('object')
-        //             expect(res.body.message).to.equal("Please provide a valid query, ie vote=up" );
-        //         });
-        // });
-
-        describe('/api/users/:username', () => {
-            it('returns details of the user required', () => {
-                return request
-                    .get('/api/users/northcoder')
-                    .expect(200)
-                    .then(res => {
-
-                        expect(res.body).be.an('object')
-                        expect(res.body.username).to.equal('northcoder')
-                        expect(res.body.name).to.equal('Awesome Northcoder')
-
-                    })
-            });
+                    expect(res.body.message).to.equal("Please provide a valid query, ie vote=up")
+                });
         })
+    })
+    /*** ERROR HANDLING */
+    it('PUT returns with error message if query value provided is invalid', () => {
 
-        describe('api/comments/:comment_id', () => {
-            it('deletes a comment if its by user northcoder', () => {
-                const commentId = docs.comments[0]._id;
+        const articleId = docs.articles[0]._id;
+        const votes = docs.articles[0].votes;
+
+        return request
+            .put(`/api/articles/${articleId}?vote=banana`)
+            .expect(400)
+            .then(res => {
+
+                expect(res.body).to.be.an('object')
+                expect(res.body.message).to.equal("Please provide a valid query format,ie vote=up or vote=down")
+
+            })
+    })
+
+    it('PUT should decrease the votes of an article by one with down query', () => {
+
+        const articleId = docs.articles[0]._id;
+        const votes = docs.articles[0].votes;
+
+        return request
+            .put(`/api/articles/${articleId}?vote=down`)
+            .expect(202)
+            .then(res => {
+
+                expect(res.body).to.be.an('object')
 
                 return request
-                    .delete(`/api/comments/${commentId}`)
-                    .expect(202)
-                    .then(res => {
+                    .get(`/api/articles/${articleId}`)
+            }).then(res => {
+                expect(res.body.article.votes).to.equal(-1);
+            })
+    })
 
+    it('PUT should increase the votes of an comment by one', () => {
 
-                        expect(res.body).to.be.an('object');
+        const commentId = docs.comments[0]._id;
+        const votes = docs.comments[0].votes;
 
-                        return request
-                            .get(`/api/comments/${commentId}`)
-                            .then(res => {
+        return request
+            .put(`/api/comments/${commentId}?vote=up`)
+            .expect(202)
+            .then(res => {
 
-                                expect(res.body).to.eql({});
-                            })
-                    })
-            });
-        })
+                expect(res.body).to.be.an('object')
 
+                return request
+                    .get(`/api/comments/${commentId}`)
+            }).then(res => {
+                expect(res.body.votes).to.equal(1);
+            })
     });
+
+    it('PUT should decrease the votes of an comment by one with down query', () => {
+
+        const commentId = docs.comments[0]._id;
+        const votes = docs.comments[0].votes;
+
+        return request
+            .put(`/api/comments/${commentId}?vote=down`)
+            .expect(202)
+            .then(res => {
+
+                expect(res.body).to.be.an('object')
+
+                return request
+                    .get(`/api/comments/${commentId}`)
+            }).then(res => {
+                expect(res.body.votes).to.equal(-1);
+            })
+    });
+
+    /*** ERROR HANDLING */
+    it.only('PUT updateCommentVote returns with error message if query provided was mispelled', () => {
+
+        const commentId = docs.comments[0]._id;
+        const votes = docs.comments[0].votes;
+
+        return request
+            .put(`/api/articles/${commentId}?ote=up`)
+            .expect(400)
+            .then(res => {
+
+                expect(res.body).to.be.an('object')
+                expect(res.body.message).to.equal("Please provide a valid query, ie vote=up")
+            });
+    })
+
+    /*** ERROR HANDLING */
+    it.only('PUT updateCommentVote returns with error message if query value provided is invalid', () => {
+
+        const commentId = docs.comments[0]._id;
+        const votes = docs.comments[0].votes;
+
+        return request
+            .put(`/api/comments/${commentId}?vote=banana`)
+            .expect(400)
+            .then(res => {
+
+                expect(res.body).to.be.an('object')
+                expect(res.body.message).to.equal("Please provide a valid query format,ie vote=up or vote=down")
+
+            })
+    })
+
+    describe('/api/users/:username', () => {
+        it('returns details of the user required', () => {
+            return request
+                .get('/api/users/northcoder')
+                .expect(200)
+                .then(res => {
+
+                    expect(res.body).be.an('object')
+                    expect(res.body.username).to.equal('northcoder')
+                    expect(res.body.name).to.equal('Awesome Northcoder')
+
+                })
+        });
+    })
+
+    describe('api/comments/:comment_id', () => {
+        it('deletes a comment if its by user northcoder', () => {
+            const commentId = docs.comments[0]._id;
+
+            return request
+                .delete(`/api/comments/${commentId}`)
+                .expect(202)
+                .then(res => {
+
+
+                    expect(res.body).to.be.an('object');
+
+                    return request
+                        .get(`/api/comments/${commentId}`)
+                        .then(res => {
+
+                            expect(res.body).to.eql({});
+                        })
+                })
+        });
+    })
+
+});
