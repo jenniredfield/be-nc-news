a8e0acbe1c74e0273752bdf2136193950e8fd546
Jennifer Bird <jenniredfield@hotmail.com>
2022-05-20 17:02:22 +0100

    Adding eslint, prettier, husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,15 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true,
-        "mocha": true,
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-multiple-empty-lines": [
-            "error",
-            { "max" : 1, 
-              "maxEOF": 1 }
-           ]
-    }
+  env: {
+    es6: true,
+    node: true,
+    mocha: true,
+  },
+  extends: "eslint:recommended",
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 1 }],
+  },
 };
